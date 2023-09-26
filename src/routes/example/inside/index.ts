970bb1f190d9957d203efea5c06065336ebe6d14
SomkamolV <somkamolv@hotmail.com>
2023-09-26 22:26:48 +0700

    Try add schema to /inside/:param1.

--- src/routes/example/inside/index.ts
+++ src/routes/example/inside/index.ts
@@ -1,9 +1,28 @@
-import { FastifyPluginAsync } from "fastify"
+import { FastifyPluginAsync } from "fastify";
 
-const deepInsideExample: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/inside', async function (request, reply) {
-    return 'this is an deep inside example'
-  })
-}
+const deepInsideExample: FastifyPluginAsync = async (
+  fastify,
+  opts
+): Promise<void> => {
+  
+  const paramsJsonSchema = {
+    type: 'object',
+    properties: {
+      par1: { type: 'string' },
+    }
+  }
+
+  fastify.route({ 
+    method: "GET", 
+    url: "/inside/:param1", 
+    schema: {params : paramsJsonSchema},
+    handler: async function (request, reply) {
+        const {param1} = request.params as { param1: string;}
+        console.log('contain param1? >',param1);
+        return 'this is an deep inside example'
+    }
+  });
+
+};
 
 export default deepInsideExample;
