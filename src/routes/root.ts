d16e31e648f5dc73769c8d4afd1eaf8647c2a515
SomkamolV <somkamolv@hotmail.com>
2023-09-26 21:46:32 +0700

    console.log(); at /

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,6 +2,8 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
+    console.log('GET /');
+    console.log('Some Support',fastify.someSupport());
     return { root: true }
   })
 }
