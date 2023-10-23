271e91bf61425591707bce8832c3c19e86002fc1
SomkamolV <somkamolv@hotmail.com>
2023-10-23 14:37:55 +0700

    add swagger

---
+++ src/plugins/swagger.ts
@@ -0,0 +1,68 @@
+import fp from "fastify-plugin";
+import fastifySwagger, { FastifySwaggerOptions } from "@fastify/swagger";
+
+/**
+ * This plugins adds swagger and swagger-ui
+ *
+ * @see https://github.com/fastify/fastify-swagger
+ * @see https://github.com/fastify/fastify-swagger-ui
+ */
+export default fp<FastifySwaggerOptions>(async (fastify) => {
+  fastify.register(fastifySwagger, {
+    swagger: {
+      info: {
+        title: "Test swagger",
+        description: "Testing the Fastify swagger API",
+        version: "0.1.0",
+      },
+      externalDocs: {
+        url: "https://swagger.io",
+        description: "Find more info here",
+      },
+      host: "localhost",
+      schemes: ["http"],
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      tags: [
+        { name: "user", description: "User related end-points" },
+        { name: "code", description: "Code related end-points" },
+      ],
+      definitions: {
+        User: {
+          type: "object",
+          required: ["id", "email"],
+          properties: {
+            id: { type: "string", format: "uuid" },
+            firstName: { type: "string" },
+            lastName: { type: "string" },
+            email: { type: "string", format: "email" },
+          },
+        },
+      },
+      securityDefinitions: {
+        apiKey: {
+          type: "apiKey",
+          name: "apiKey",
+          in: "header",
+        },
+      },
+    },
+  });
+
+
+  await fastify.register(require('@fastify/swagger-ui'), {
+    routePrefix: '/documentation',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) { next() },
+      preHandler: function (request, reply, next) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+    transformSpecificationClone: true
+  })
+});
